24f453efa3e4b30916a323513ed20a67388f7f12
shaneword <shane.word@farmersinsurance.com>
2015-09-18 15:40:59 -0700

    refactor(Site): Updated build to dynamic approach

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,13 @@
 var gulp = require('gulp');
-var $ = require("gulp-load-plugins")({lazy:false, pattern: ['gulp-*', 'gulp.*', 'del*', 'handlebars*', 'front*', 'marked*']});
+var $ = require("gulp-load-plugins")({lazy:false, pattern: ['gulp-*', 'gulp.*', 'del*', 'handlebars*', 'front*', 
+                    'helper-markdown*', 'jsonfile', 'merge-stream', 'event-stream']});
 var pkg = require('./package.json');
 var Path = require('path');
 var fs = require('fs');
-var es = require('event-stream');
 var url = require('url');
-var browserSync = require('browser-sync');
+var _ = require('lodash');
+var _s = require('underscore.string');
+var cheerio = require('cheerio');
 
 var paths = {
     templates: './src/templates',
@@ -19,9 +21,56 @@ var paths = {
     content: './src/content'
 };
 
+
 var devMode = true;
 
-gulp.task('partials', [], function() {
+var siteData = {pages:[], posts:[], devMode:devMode};
+
+gulp.task('handlebarshelpers', function() {
+  $.handlebars.registerHelper('markdown', require('helper-markdown'));
+
+});
+
+gulp.task('sitedata', ['loadsitedata'], function() {
+    siteData.posts.sort(postOrderCompare);
+    setPostNextPrevious(siteData.posts);
+    guaranteeDir(paths.build);
+    $.jsonfile.writeFileSync(Path.join(paths.build, 'sitedata.json'), siteData);
+    return gulp.src(Path.join(paths.build, 'sitedata.json'));
+});
+
+gulp.task('loadsitedata', [], function() {
+    var posts = gulp.src(Path.join(paths.posts, '**/**.hbs'))
+        .pipe($.eventStream.map(function(fileData, cb) {
+            var content = $.frontMatter(String(fileData.contents));
+            var body = content.body;
+            content = content.attributes;
+            var filePath = Path.parse(fileData.path);
+            var relativeDir = Path.relative(paths.posts, filePath.dir);
+            var postData = {id:filePath.name, path:relativeDir, title:content.title || filePath.name,  
+                url:content.url || false, date:content.date || generateDate(fileData.path), tags:content.tags || '', 
+                categories:content.categories || '', published:content.published || false, excerpt:false, 
+                nextpostid:false, previouspostid:false, author:content.author || 'Shane' };
+            postData.excerpt = postData.excerpt || getExcerptByMoreTag(body);
+            siteData.posts.push(postData);
+            cb(null, fileData);
+          }));
+    var pages = gulp.src(Path.join(paths.pages, '**/**.hbs'))
+        .pipe($.eventStream.map(function(fileData, cb) {
+            var content = $.frontMatter(String(fileData.contents));
+            content = content.attributes;
+            var filePath = Path.parse(fileData.path);
+            var relativeDir = Path.relative(paths.pages, filePath.dir);
+            var pageData = {id:filePath.name, path:relativeDir, title:content.title || filePath.name, 
+                url:content.url || null, date:content.date || generateDate(fileData.path), tags:content.tags || '', 
+                categories:content.categories || '', published:content.published || false};
+            siteData.pages.push(pageData);
+            cb(null, fileData);
+          }));
+    return $.mergeStream(posts, pages);
+});
+
+gulp.task('partials', ['handlebarshelpers', 'sitedata'], function() {
   return gulp.src(Path.join(paths.partials, '**/**.hbs'))
     .pipe($.tap(function(file) {
       var template = file.contents.toString();
@@ -31,30 +80,44 @@ gulp.task('partials', [], function() {
     }))
 });
 
-gulp.task('pages', ['partials', 'less'], function() {
+gulp.task('pages', ['partials', 'handlebarshelpers', 'sitedata'], function() {
     return gulp.src(Path.join(paths.pages, '**/**.hbs'))
         .pipe($.data(function(file){
             var content = $.frontMatter(String(file.contents));
             file.contents = new Buffer(content.body);
-//            file.contents = new Buffer($.marked(content.body)); no markup for now
             return content.attributes;
         }))
-        .pipe(es.map(function(file, cb) {
-            file.data.body = String(file.contents);
-            file.data.devMode = devMode;
-
-//            var templateName = file.data.template || 'default.hbs';
-//            var templateData = String(fs.readFileSync(Path.join(paths.templates, templateName)));
-//            console.log("templateData for: "+templateName+" is: "+templateData);
-//            var template = $.handlebars.compile(templateData);
-            var template = $.handlebars.compile(file.data.body);
-            var html = template(file.data, {});
-//            console.log("html : "+html);
+        .pipe($.eventStream.map(function(file, cb) {
+            var filePath = Path.parse(file.path);
+            var pageInfo = _.findWhere(siteData.pages, {id:filePath.name});
+            pageInfo = _.extend({}, file.data, pageInfo);
+            var template = $.handlebars.compile(String(file.contents));
+            var html = template(_.extend({}, pageInfo, siteData), {});
             file.contents = new Buffer(html, "utf-8");
             cb(null, file);
         }))
         .pipe($.rename({extname: ".html"}))
         .pipe(gulp.dest(paths.build));
+});
+
+gulp.task('posts', ['partials', 'handlebarshelpers', 'sitedata'], function() {
+    return gulp.src(Path.join(paths.posts, '**/**.hbs'))
+        .pipe($.data(function(file){
+            var content = $.frontMatter(String(file.contents));
+            file.contents = new Buffer(content.body);
+            return content.attributes;
+        }))
+        .pipe($.eventStream.map(function(file, cb) {
+            var filePath = Path.parse(file.path);
+            var postInfo = _.findWhere(siteData.posts, {id:filePath.name});
+            postInfo = _.extend({}, file.data, postInfo);
+            var template = $.handlebars.compile(String(file.contents));
+            var html = template(_.extend({}, postInfo, siteData), {});
+            file.contents = new Buffer(html, "utf-8");
+            cb(null, file);
+        }))
+        .pipe($.rename({extname: ".html"}))
+        .pipe(gulp.dest(Path.join(paths.build, 'posts')));
 });
 
 gulp.task('less', [], function () {
@@ -88,12 +151,60 @@ gulp.task('serve', ['build', 'watch'], function () {
 
 gulp.task('watch', function () {
     gulp.watch(Path.join(paths.less, '**/**.less'), ['less']);
-    gulp.watch(Path.join(paths.pages, '**/**.md'), ['pages']);
-    gulp.watch(Path.join(paths.templates, '**/**.hbs'), ['pages']);
+    gulp.watch(Path.join(paths.pages, '**/**.hbs'), ['pages']);
+    gulp.watch(Path.join(paths.templates, '**/**.hbs'), ['pages', 'posts']);
+    gulp.watch(Path.join(paths.posts, '**/**.hbs'), ['posts']);
     gulp.watch(Path.join(paths.assets, '**'), ['assets']);
 });
 
-gulp.task('build', ['less',  'pages'
+gulp.task('build', ['less',  'pages', 'posts', 'assets'
                     ]);
 
 gulp.task('default', ['build']);
+
+function generateDate(file) {
+    var stat = fs.statSync(file);
+    return stat.birthtime;
+}
+
+function postOrderCompare(postA, postB) {
+    if (postA.date > postB.date) {
+        return -1;
+    }
+    if (postA.date > postB.date) {
+        return 1;
+    }
+    return 0;
+}
+
+function setPostNextPrevious(posts) {
+    for (var i =0;i<posts.length;i++) {
+        if (i > 0) {
+            posts[i].previouspostid = posts[i-1].id;
+        }
+        if (i + 1 < posts.length) {
+            posts[i].nextpostid = posts[i+1].id;
+        }
+    }
+}
+
+function guaranteeDir(dir) {    
+    try {
+        fs.accessSync(Path.join(dir, ''), fs.R_OK | fs.W_OK);
+    } catch (err) {
+        fs.mkdirSync(Path.join(dir, ''));
+    }
+}
+
+function getExcerptByMoreTag(html) {
+    var excerpt = false;
+    var startString = '<!--start excerpt-->';
+    var startPos = html.indexOf(startString);
+    var endPos = html.indexOf('<!--end excerpt-->');
+    if (startPos > -1 && endPos > -1) {
+        excerpt = html.slice(startPos + startString.length, endPos);
+        var $ = cheerio.load(excerpt);
+        excerpt = $('*').text();
+    }
+    return excerpt;
+}
